1ab2b8d57caa07f712347fd8b3d06bbc88d715f9
Mehmet Hazan <mhazan20@gmail.com>
2026-04-01 19:50:10 +0000

    tammammi

---
+++ my-first-project/cyber-assessment/5-my_github.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+username=sys.argv[1]
+token=sys.argv[2]
+
+response=requests.get('https://api.github.com/user',auth=(username,token))
+print(response.json().get('id'))
